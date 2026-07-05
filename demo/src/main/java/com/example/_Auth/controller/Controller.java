fee937d275979b36724bd76a63815820cda31b6d
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-06 01:22:50 +0200

    store user detail in Oauth2User

--- demo/src/main/java/com/example/_Auth/controller/Controller.java
+++ demo/src/main/java/com/example/_Auth/controller/Controller.java
@@ -1,8 +1,16 @@
 package com.example._Auth.controller;
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -24,4 +32,18 @@ public class Controller {
         return "Hey Api";
     }
 
+    @GetMapping("/me")
+    public Map<String, Object> getUser(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2User user = (OAuth2User) authentication.getPrincipal();
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("name", user.getAttribute("name"));
+        response.put("email", user.getAttribute("email"));
+        response.put("picture", user.getAttribute("picture"));
+
+        return response;
+    }
 }
