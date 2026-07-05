cca4254595fc364f9e53273c779c20732e0d0919
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-06 00:58:44 +0200

    add security configuration

--- demo/src/main/java/com/example/_Auth/config/SecurityConfig.java
+++ demo/src/main/java/com/example/_Auth/config/SecurityConfig.java
@@ -1,12 +1,27 @@
 package com.example._Auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.csrf( csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+
+    }
 
 }
